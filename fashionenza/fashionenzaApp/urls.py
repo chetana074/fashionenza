1387b5d3e1e2e8f3bc4b35cc68c790113c9f1294
Chetana Nagare <82255820+chetana074@users.noreply.github.com>
2022-12-30 18:22:08 +0530

    committing files

---
+++ fashionenza/fashionenzaApp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from fashionenzaApp import views
+
+urlpatterns = [
+    path('', views.index, name='index'), #empty path redirectes to index page
+    path('index', views.index, name='index'), #path with 'index' redirectes to index page
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact')
+]
