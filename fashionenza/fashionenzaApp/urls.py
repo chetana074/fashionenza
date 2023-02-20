7e274d981d014763b453b25731c812aa963c1d10
Chetana Nagare <82255820+chetana074@users.noreply.github.com>
2023-02-21 01:11:18 +0530

    Add files via upload

--- fashionenza/fashionenzaApp/urls.py
+++ fashionenza/fashionenzaApp/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from fashionenzaApp import views
 
 urlpatterns = [
-    path('', views.index, name='index'), #empty path redirectes to index page
-    path('index', views.index, name='index'), #path with 'index' redirectes to index page
+    path('', views.welcome, name='welcome'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('home', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('userInfo', views.userInfo, name='userInfo'),
-    path('garments', views.garments, name='garments')
+    path('tryon', views.tryon, name='tryon'),
+    path('userInfo', views.userInfo, name='userInfo')
 ]
--- fashionenza/fashionenzaApp/views.py
+++ fashionenza/fashionenzaApp/views.py
@@ -1,7 +1,76 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-def index(request):
-    return render(request, 'index.html')
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def validate_password(s):
+    l, u, p, d = 0, 0, 0, 0
+    if (len(s) >= 8):
+        for i in s:
+            
+            # counting lowercase alphabets
+            if (i.islower()):
+                l+=1           
+    
+            # counting uppercase alphabets
+            if (i.isupper()):
+                u+=1           
+    
+            # counting digits
+            if (i.isdigit()):
+                d+=1           
+    
+            # counting the mentioned special characters
+            if(i=='@'or i=='$' or i=='_'):
+                p+=1          
+
+    if (l>=1 and u>=1 and p>=1 and d>=1 and l+p+u+d==len(s)):
+        return True
+    else:
+        return False
+    
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        newUser = User.objects.create_user(username, email, password1)
+        newUser.first_name = fname
+        newUser.last_name = lname
+        newUser.save()
+
+        return redirect('signin')
+
+    return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+
+        user = authenticate(username=username, password=password1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, 'home.html', {'fname':fname})
+        else:
+            return redirect('welcome')
+
+    return render(request, 'signin.html')
+
+def signout(request):
+    logout(request)
+    return redirect('welcome')
+
+def home(request):
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -9,8 +78,8 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
+def tryon(request):
+    return render(request, 'tryon.html')
+
 def userInfo(request):
     return render(request, 'userInfo.html')
-
-def garments(request):
-    return render(request, 'garments.html')
