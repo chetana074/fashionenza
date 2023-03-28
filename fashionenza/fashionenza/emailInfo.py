43ee0ac5bc0a836c577d3c921f1aa25f313a4544
Chetana Nagare <82255820+chetana074@users.noreply.github.com>
2023-03-28 12:51:48 +0530

    Add files via upload

---
+++ fashionenza/fashionenza/emailInfo.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS = True #settings control whether a secure connection is used
+EMAIL_HOST = "smtp.gmail.com" #host to use for sending email
+EMAIL_HOST_USER = "fashionenza3@gmail.com" #acount to which email will be sent
+EMAIL_HOST_PASSWORD = "Cummins@123"
+EMAIL_PORT = 587 #port used
+
+#then mention email settings in settings.py
