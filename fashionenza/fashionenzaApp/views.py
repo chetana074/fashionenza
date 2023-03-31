7357a439dc56dccf8b7692e70392289881400796
vinaya <vinaya.patil@cumminscollege.in>
2023-03-31 16:37:23 +0530

    committing updated files

--- fashionenza/fashionenzaApp/views.py
+++ fashionenza/fashionenzaApp/views.py
@@ -6,8 +6,6 @@ from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse
 from fashionenzaApp import imagecapture
 
-loggedIn = False
-
 def welcome(request):
     return render(request, 'welcome.html')
 
@@ -20,20 +18,20 @@ def signup(request):
         password = request.POST['password1']
 
         if User.objects.filter(username=username).exists():
-            messages.error(request, "This username is taken")
+            messages.warning(request, "This username is taken")
             return render(request, 'signup.html')
         elif User.objects.filter(email=email).exists():
-            messages.error(request, "This email is taken")
+            messages.warning(request, "This email is taken")
             return render(request, 'signup.html')
         else:
             newUser = User.objects.create_user(username, email, password)
             newUser.first_name = fname
             newUser.last_name = lname
             newUser.save()
-            messages.success(request, "User registered successfully!!!")
             return redirect('signin')
         
-    return render(request, 'signup.html')
+    if request.method == "GET": 
+        return render(request, 'signup.html')
 
 def signin(request):
     if request.method == "POST":
@@ -46,65 +44,53 @@ def signin(request):
             login(request, user)
             username = user.username
             fname = user.first_name
-            loggedIn = True
-            return render(request, 'home.html', {'fname':fname, 'username':username, 'loggedIn':loggedIn})
+            # print('loggedIn signin', loggedIn)
+            # return render(request, 'home.html', {'fname':fname, 'username':username, 'loggedIn':loggedIn})
+            messages.success(request, "User loggedIn!!!")
+            return redirect('home')
         else:
-            messages.error(request, "User is not registered!!! Please sign up first.")
-            return redirect('welcome')
-    return render(request, 'signin.html')
+            messages.warning(request, "Wrong credentials!! If user is not registered, please sign up first.")
+            return redirect('signin')
+        
+    if request.method == "GET":
+        return render(request, 'signin.html')
 
 def signout(request):
-    loggedIn = False
     logout(request)
     return redirect('welcome')
 
 def home(request):
-    print("loggedIn" + loggedIn)
-    if(loggedIn):
+    if request.method == "GET":
         return render(request, 'home.html')
-    else:
-        return redirect('welcome')
-    
+
 def about(request):
-    if(loggedIn):
+    if request.method == "GET":
         return render(request, 'about.html')
-    else:
-        return redirect('welcome')
 
 def contact(request):
-    if(loggedIn):
+    if request.method == "GET":
         return render(request, 'contact.html')
-    else:
-        return redirect('welcome')
 
 
 def upload(request):
-    if request.method=='POST' and 'garm' in request.POST:
-        upload=request.FILES['gar']
-        fss=FileSystemStorage(location='Garment_by_user')
-        fss.save(upload.name, upload)
-    if request.method=='POST' and 'your' in request.POST: 
+    if request.method=='POST'and 'your' in request.POST:
+       upload=request.FILES['gar']
+       fss=FileSystemStorage(location='Garment_by_user')
+       fss.save(upload.name, upload)
+    if request.method=='POST': 
         up=request.FILES['you']
         fss=FileSystemStorage(location='User_photo')
         fss.save(up.name, up) 
-    # if request.method=='POST' and 'capture' in request.POST: 
-    #     imagecapture.dummy()
     return render(request,'upload.html')
 
 
 def garments(request):
-    if(loggedIn):
-        return(request,'garments.html')
-    else:
-        return redirect('welcome')
+    return(request,'garments.html')
 
-def upload(request):
-    if request.method == 'POST' and 'capture' in request.POST: 
-        text = imagecapture.dummy()
-    return render(request,'upload.html') 
+# def upload(request):
+#     if request.method == 'POST' and 'capture' in request.POST: 
+#         text = imagecapture.dummy()
+#     return render(request,'upload.html') 
 
 def userInfo(request):
-    if(loggedIn):
-        return render(request, 'userInfo.html')
-    else:
-        return redirect('welcome')
+    return render(request, 'userInfo.html')
