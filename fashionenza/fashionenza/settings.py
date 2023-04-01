602fe1b383032b409d0b7d40f39357ea49af0083
chetana074 <chetana.nagare@cumminscollege.in>
2023-04-01 14:43:26 +0530

    committing changes done for authentication

--- fashionenza/fashionenza/settings.py
+++ fashionenza/fashionenza/settings.py
@@ -131,6 +131,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+LOGIN_REDIRECT_URL = 'home' #redirect to home after login
+LOGIN_URL = '/' #redirect to welcome if not logged in
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
